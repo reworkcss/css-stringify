8240dcffff9b01a8faad70f3ef003e0eebc587ce
TJ Holowaychuk <tj@vision-media.ca>
2012-07-18 17:00:43 -0700

    add initial implementation

--- examples/dialog.js
+++ examples/dialog.js
@@ -3,9 +3,10 @@
  * Module dependencies.
  */
 
-var parse = require('..')
+var parse = require('css-parse')
+  , stringify = require('..')
   , fs = require('fs')
   , read = fs.readFileSync
   , css = read('examples/dialog.css', 'utf8');
 
-console.log(JSON.stringify(parse(css), null, 2));
+console.log(stringify(parse(css)));
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
 module.exports = function(node){
-  
+  return node.stylesheet.rules.map(rule).join('\n');
 };
+
+function rule(rule) {
+  return rule.selector
+    + ' {\n'
+    + rule.declarations.map(declaration).join(';\n')
+    + '\n}';
+}
+
+function declaration(decl) {
+  return decl.property + ': ' + decl.value;
+}
