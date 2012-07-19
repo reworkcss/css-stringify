7d46195226075f6745e2f4f73d4bbf3278b75118
TJ Holowaychuk <tj@vision-media.ca>
2012-07-18 22:49:47 -0700

    add keyframe support

--- index.js
+++ index.js
@@ -11,9 +11,23 @@
 module.exports = function(node, options){
   options = options || {};
   return options.compress
-    ? node.stylesheet.rules.map(rule(options)).join('')
-    : node.stylesheet.rules.map(rule(options)).join('\n\n');
+    ? node.stylesheet.rules.map(visit(options)).join('')
+    : node.stylesheet.rules.map(visit(options)).join('\n\n');
 };
+
+/**
+ * Visit rule nodes.
+ */
+
+function visit(options) {
+  var _rule = rule(options);
+  var _keyframes = keyframes(options);
+  return function(node){
+    if (node.keyframes) return _keyframes(node);
+    if (node.import) return atimport(node);
+    return _rule(options);
+  }
+}
 
 /**
  * Compile import.
@@ -24,14 +38,63 @@ function atimport(rule) {
 }
 
 /**
+ * Compile keyframes.
+ */
+
+function keyframes(options) {
+  if (options.compress) {
+    return function(keyframes){
+      return '@'
+      + (keyframes.vendor || '')
+      + 'keyframes '
+      + keyframes.name
+      + '{'
+      + keyframes.keyframes.map(keyframe(options)).join('')
+      + '}';
+    }
+  }
+
+  return function(keyframes){
+    return '@'
+    + (keyframes.vendor || '')
+    + 'keyframes '
+    + keyframes.name
+    + ' {\n'
+    + keyframes.keyframes.map(keyframe(options)).join('\n')
+    + '}';
+  }
+}
+
+/**
+ * Compile keyframe.
+ */
+
+function keyframe(options) {
+  if (options.compress) {
+    return function(keyframe){
+      return keyframe.values.join(',')
+        + '{'
+        + keyframe.declarations.map(declaration(options)).join(';')
+        + '}'
+    }
+  }
+
+  return function(keyframe){
+    return '  '
+      + keyframe.values.join(', ')
+      + ' {\n'
+      + keyframe.declarations.map(indent(declaration(options))).join(';')
+      + '\n  }\n'
+  }
+}
+
+/**
  * Compile rule.
  */
 
 function rule(options) {
   if (options.compress) {
-    return function(rule) {
-      if (rule.import) return atimport(rule);
-
+    return function(rule){
       return rule.selector
         + '{'
         + rule.declarations.map(declaration(options)).join(';')
@@ -39,9 +102,7 @@ function rule(options) {
     }
   }
 
-  return function(rule) {
-    if (rule.import) return atimport(rule);
-
+  return function(rule){
     return rule.selector
       + ' {\n'
       + rule.declarations.map(declaration(options)).join('\n')
@@ -62,5 +123,15 @@ function declaration(options) {
 
   return function(decl){
     return '  ' + decl.property + ': ' + decl.value + ';';
+  }
+}
+
+/**
+ * Indent.
+ */
+
+function indent(fn) {
+  return function(val){
+    return '  ' + fn(val);
   }
 }
