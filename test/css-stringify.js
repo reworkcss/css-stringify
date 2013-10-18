7347b8b4c3facf5127d1d3a783b08ca314f210e9
Brett Stimmerman <bstimmerman@twitter.com>
2013-10-18 11:55:24 -0700

    Fix whitespace and indentation in the Compressed compiler.
    Before this change the Compressed compiler errored when processing @page and
    @supports blocks, and the tests did not cover this.
    
    This change fixes @page and @supports compression in the Compressed compiler,
    fixes the tests to fully flex the Compressed compiler, and adds missing fixtures
    to flesh out the test suite.

--- test/css-stringify.js
+++ test/css-stringify.js
@@ -12,13 +12,16 @@ var stringify = require('..')
 
 describe('stringify(obj)', function(){
   readdir('test/cases').forEach(function(file){
-    var compress = ~file.indexOf('compress');
-    file = path.basename(file, '.css');
-    it('should stringify ' + file, function(){
-      var css = read(path.join('test', 'cases', file + '.css'), 'utf8');
-      if (compress) file = file.replace('.compress', '');
+    var compress = ~file.indexOf('.compressed');
+    it('should stringify ' + path.basename(file), function(){
+      var expect;
+      if (compress) {
+        expect = read(path.join('test', 'cases', file), 'utf8');
+        file = file.replace('.compressed', '');
+      }
+      var css = read(path.join('test', 'cases', file), 'utf8');
       var ret = stringify(parse(css), { compress: compress });
-      ret.trim().should.equal(css.trim());
+      ret.trim().should.equal((expect || css).trim());
     });
   });
 });
